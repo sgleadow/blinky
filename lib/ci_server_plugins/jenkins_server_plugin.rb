52c5bfa5d2638f6165241ebd56cf3ca715532bf5
Stewart Gleadow <sgleadow@thoughtworks.com>
2011-05-11 11:32:10 +1000

    Cleaning up the code a bit

--- lib/ci_server_plugins/jenkins_server_plugin.rb
+++ lib/ci_server_plugins/jenkins_server_plugin.rb
@@ -3,13 +3,19 @@ require 'nokogiri'
 
 module Blinky
   module JenkingsCiServer
+    
+    def create_ci_server_location
+      server_location = ENV['BLINKY_CI_SERVER'] || "localhost"
+      server_port = ENV['BLINK_CI_PORT'] || "8080"
+      server_path = ENV['CI_SERVER_PATH'] || ""
+      server_path = server_path + "/" unless server_path.end_with? "/"
+      "http://#{server_location}:#{server_port}/#{server_path}cc.xml"
+    end
+    
     def watch_jenkins_server
+      ci_server = create_ci_server_location
       
       while (true)
-        server_location = ENV['BLINKY_CI_SERVER'] || "localhost"
-        server_port = ENV['BLINK_CI_PORT'] || "8080"
-        server_path = ENV['CI_SERVER_PATH'] || ""
-        ci_server = "http://#{server_location}:#{server_port}/sss#{server_path}cc.xml"
         
         r = HTTParty.get ci_server
         
@@ -22,7 +28,11 @@ module Blinky
           puts proj
        
           if (proj.attribute('activity').to_str == "Sleeping")
-            failure! unless proj.attribute('lastBuildStatus').to_str == "Success"
+            if (proj.attribute('lastBuildStatus').to_str == "Success")
+              success!
+            else
+              failure!
+            end
           else
             building!
           end
