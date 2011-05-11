7160e850a0d33c2a6e985d21f229b131b77a25a0
Stewart Gleadow <sgleadow@thoughtworks.com>
2011-05-11 11:00:18 +1000

    Addig warning light for no connection

--- lib/ci_server_plugins/jenkins_server_plugin.rb
+++ lib/ci_server_plugins/jenkins_server_plugin.rb
@@ -9,18 +9,23 @@ module Blinky
         server_location = ENV['BLINKY_CI_SERVER'] || "localhost"
         server_port = ENV['BLINK_CI_PORT'] || "8080"
         server_path = ENV['CI_SERVER_PATH'] || ""
-        ci_server = "http://#{server_location}:#{server_port}/#{server_path}cc.xml"
+        ci_server = "http://#{server_location}:#{server_port}/sss#{server_path}cc.xml"
         
-        r = HTTParty.get "http://ci-mac.local:8080/cc.xml"
-        xml = Nokogiri::XML(r.body)
-        proj = xml.xpath("//Project[@name='playup-live-ios']")
-      
-        puts proj
-       
-        if (proj.attribute('activity').to_str == "Sleeping")
-          failure! unless proj.attribute('lastBuildStatus').to_str == "Success"
+        r = HTTParty.get ci_server
+        
+        if (r.code >= 400)
+          warning!
         else
-          building!
+          xml = Nokogiri::XML(r.body)
+          proj = xml.xpath("//Project[@name='playup-live-ios']")
+      
+          puts proj
+       
+          if (proj.attribute('activity').to_str == "Sleeping")
+            failure! unless proj.attribute('lastBuildStatus').to_str == "Success"
+          else
+            building!
+          end
         end
         
         sleep(1)
